de78ef168ec04b0cf76db2514c9a00a90b589a85
Rodrigo <r.rochacampos10@gmail.com>
2024-10-04 20:36:47 -0300

    add arquivos html, css, js e imagens

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Causaganha Advogados
+# Causa Ganha Advogados
+
+Bem-vindo ao repositório do **Causa Ganha Advogados**! Este projeto é uma página web desenvolvida para apresentar os serviços de um escritório de advocacia fictício.
+
+## Descrição do projeto
+
+Este projeto inclui informações sobre os serviços oferecidos, história do escritório, depoimentos de clientes e formas de contato. O design da página é responsivo, garantindo uma ótima experiência para os usuários em diferentes dispositivos.
+
+## Conteúdo
+
+Todos os conteúdos apresentados na página (nome do escritório, história, serviços oferecidos, depoimentos, fotos etc.) foram gerados por inteligência artificial.
+
+### Acesse a página
+
+Para visualizar a página em desenvolvimento, [clique aqui](#).
---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.navbar-nav .nav-link').forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        let navbarToggler = document.querySelector('.navbar-collapse');
+        let bsCollapse = new bootstrap.Collapse(navbarToggler, { toggle: false });
+        bsCollapse.hide();
+    });
+
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        let targetId = this.getAttribute('href').substring(1);
+        let targetElement = document.getElementById(targetId);
+
+        let offsetPosition = targetElement.offsetTop - 24;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
